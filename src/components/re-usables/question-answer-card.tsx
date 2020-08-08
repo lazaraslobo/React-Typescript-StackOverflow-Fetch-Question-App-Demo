e86e558db22cb476e892fd8b79632660d7a52b28
Deepak Lobo <deepaklobo16@gmail.com>
2020-08-08 22:36:33 +0530

    css fixes

--- src/components/re-usables/question-answer-card.tsx
+++ src/components/re-usables/question-answer-card.tsx
@@ -4,25 +4,25 @@ import {options} from '../Grid'
 import {Grid} from '@material-ui/core';
 
 export interface QAcompPropsInterface {
-        "tags": [],
-        "owner": { 
-            "reputation": number,
-            "user_id": number,
-            "user_type": string,
-            "profile_image": string,
-            "display_name": string,
-            "link":  string
-        },
-        "is_answered": false,
-        "view_count": number,
-        "answer_count": number,
-        "score": number,
-        "last_activity_date": number,
-        "creation_date": number,
-        "question_id": number,
-        "content_license": string,
-        "link": string,
-        "title": string 
+    "tags": [],
+    "owner": { 
+        "reputation": number,
+        "user_id": number,
+        "user_type": string,
+        "profile_image": string,
+        "display_name": string,
+        "link":  string
+    },
+    "is_answered": false,
+    "view_count": number,
+    "answer_count": number,
+    "score": number,
+    "last_activity_date": number,
+    "creation_date": number,
+    "question_id": number,
+    "content_license": string,
+    "link": string,
+    "title": string 
 }
 
 
@@ -30,29 +30,46 @@ const QuestionAnswerCard = (props : QAcompPropsInterface) =>{
     return (
         <Wrapper>
             <Grid {...options.contRowStartCenter}>
-                    <Grid item xs={3} sm={2} lg={2} md={2} xl={2}>
-                        <ProfileImage src={props.owner.profile_image ? props.owner.profile_image : require("../../assets/user-profile.png")} alt={props.owner.display_name}/>
-                    </Grid>
-                    <Grid item xs={9}>
-                        <RichText>{props.owner.display_name}</RichText>
-                    </Grid>
+                <Grid item xs={3} sm={2} lg={2} md={2} xl={2}>
+                    <ProfileImage src={props.owner.profile_image ? props.owner.profile_image : require("../../assets/user-profile.png")} alt={props.owner.display_name}/>
+                </Grid>
+                <Grid item xs={9}>
+                    <RichText>{props.owner.display_name}</RichText>
+                </Grid>
             </Grid>
             <Grid {...options.contRowStartCenter}>
-                    <CardTitle>``{props.title}``</CardTitle>
+                <CardTitle>``{props.title}``</CardTitle>
             </Grid>
             <Grid {...options.contRowStartCenter}>
-                <Grid item xs={10} sm={10} md={8} lg={8} xl={8}>
-                    <Grid {...options.contRowStartCenter}>
-                        <Grid item xs={1}>
+                <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                    <Grid {...options.contRowCenterCenter}>
+                        <Grid item xs={2}>
                             <span className="material-icons WT">people</span>
                         </Grid>
-                        <Grid item xs={1}>
+                        <Grid item xs={2}>
                             <RichText>{props.answer_count}</RichText>
                         </Grid>
                     </Grid>
                 </Grid>
-                <Grid item xs={2}>
-                    <span className="material-icons WT">remove_red_eye</span>
+                <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                    <Grid {...options.contRowCenterCenter}>
+                        <Grid item xs={2}>
+                            <span className="material-icons WT">comment</span>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <RichText>{props.answer_count}</RichText>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                    <Grid {...options.contRowCenterCenter}>
+                        <Grid item xs={2}>
+                            <span className="material-icons WT">remove_red_eye</span>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <RichText>{props.view_count}</RichText>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </Wrapper>
