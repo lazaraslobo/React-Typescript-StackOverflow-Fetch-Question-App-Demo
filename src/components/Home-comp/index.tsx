66402546486bef5027f1eff75590c541da35e957
Deepak Lobo <deepaklobo16@gmail.com>
2020-08-08 15:35:18 +0530

    removed unwanted files

---
+++ src/components/Home-comp/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class HomeComponent extends React.Component{
+    render(){
+        return(
+            <h1>Home component ...</h1>
+        )
+    }
+}
+
+
+export default HomeComponent;
