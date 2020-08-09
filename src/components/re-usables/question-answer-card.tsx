0c3059f2e86347f2d3042457c87b809293e10b58
Deepak Lobo <deepaklobo16@gmail.com>
2020-08-09 16:39:31 +0530

    minor fixes

--- src/components/re-usables/question-answer-card.tsx
+++ src/components/re-usables/question-answer-card.tsx
@@ -11,7 +11,7 @@ interface interfaceBar{
 
 const CreateQaInfoBar = (barProps : interfaceBar) =>{
     return (
-        <Grid item xs={4} sm={4} md={4} lg={3} xl={3}>
+        <Grid item xs={3} sm={3} md={4} lg={3} xl={3}>
             <Grid {...options.contRowStartCenter}>
                 <Grid item xs={3}>
                     <span className="material-icons WT">{barProps.iconName}</span>
